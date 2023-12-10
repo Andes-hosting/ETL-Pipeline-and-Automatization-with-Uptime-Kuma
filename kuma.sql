be936d7a34c8803f94c860b4c120fa6d8c9d2b37
Gonzalo Moyano <gonzalomhenriquez13@gmail.com>
2023-12-10 16:33:48 -0300

    Improves

---
+++ kuma.sql
@@ -0,0 +1,20 @@
+-- CREATE SCHEMA 
+CREATE SCHEMA kuma;
+
+-- TABLES FOR MINECRAFT LOGS
+
+CREATE TABLE kuma.nodes (
+	id SERIAL PRIMARY KEY,
+	node_id text NOT NULL,
+	avg_ping text NOT NULL,
+	uptime_24hours text NOT NULL,
+	creation_time timestamptz NOT NULL
+);
+
+CREATE TABLE kuma.servers (
+	id SERIAL PRIMARY KEY,
+	server_identifier text NOT NULL,
+	avg_ping text NOT NULL,
+	uptime_24hours text NOT NULL,
+	creation_time timestamptz NOT NULL
+);
