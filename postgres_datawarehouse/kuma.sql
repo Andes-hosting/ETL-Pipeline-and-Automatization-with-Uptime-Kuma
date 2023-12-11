093f5588857621d0f9a8d2991db2c5329342b279
Gonzalo Moyano <gonzalomhenriquez13@gmail.com>
2023-12-11 12:46:10 -0300

    Change login method

--- postgres_datawarehouse/kuma.sql
+++ postgres_datawarehouse/kuma.sql
@@ -5,16 +5,16 @@ CREATE SCHEMA kuma;
 
 CREATE TABLE kuma.nodes (
 	id SERIAL PRIMARY KEY,
-	node_id text NOT NULL,
-	avg_ping text NOT NULL,
-	uptime_24hours text NOT NULL,
+	node_id int NOT NULL,
+	avg_ping int NOT NULL,
+	uptime_24hours int NOT NULL,
 	creation_time timestamptz NOT NULL
 );
 
 CREATE TABLE kuma.servers (
 	id SERIAL PRIMARY KEY,
 	server_identifier text NOT NULL,
-	avg_ping text NOT NULL,
-	uptime_24hours text NOT NULL,
+	avg_ping int NOT NULL,
+	uptime_24hours int NOT NULL,
 	creation_time timestamptz NOT NULL
 );
